ece4b2eedc8b4b1e6de4f54f22362af8957bbc83
alanAustin <alantahseen1@gmail.com>
2026-08-30 01:30:41 +0300

    curd on userController is done

---
+++ backend/app/Http/Requests/Api/userRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\api;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class userRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        if ($this->isMethod('POST')) {
+            return [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string|min:8|confirmed',
+                'role' => 'required|string|exists:roles,name',
+                'permissions' => 'sometimes|array',
+                'permissions.*' => 'string|exists:permissions,name'
+            ];
+        } elseif ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
+            return [
+                'name' => 'sometimes|required|string|max:255',
+                'email' => 'sometimes|required|email|unique:users,email,' . $this->route('id'),
+                'password' => 'sometimes|required|string|min:8|confirmed',
+                'role' => 'sometimes|required|string|exists:roles,name',
+                'permissions' => 'sometimes|array',
+                'permissions.*' => 'string|exists:permissions,name'
+            ];
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::prefix('admin')->middleware(['auth:sanctum'])->group(function () {
+
+    route::apiResource('/users', UserController::class);
+});
