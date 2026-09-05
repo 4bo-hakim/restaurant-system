f5c2c32b8b665eb5a362e8b3163fb2c56933af16
alanAustin <alantahseen1@gmail.com>
2026-09-05 23:41:05 +0300

    adding telescop package to test the speed of the system

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/test', function () {
+    return \App\Models\Food::limit(20)->get();
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
